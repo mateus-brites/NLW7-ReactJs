49667ac0fe7a5f2106172ee4dc7720fd24e55df3
mateus-brites <mateusmmo15@gmail.com>
2022-02-01 23:43:04 -0300

    configuraçao do firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+
+import { getAuth } from 'firebase/auth';
+import {getDatabase } from 'firebase/database';
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase(app);
+export const auth = getAuth(app);
