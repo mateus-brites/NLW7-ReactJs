d120f488d86f096849ddb1a89b67ba18dd9d021a
mateus-brites <mateusmmo15@gmail.com>
2022-02-02 23:40:16 -0300

    comecei a fazer o css da aplicaçao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,26 +2,28 @@ import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
 
 export function Home() {
     return (
-        <div>
+        <div id='page-auth'>
             <aside>
                 <img src={ilustrationImg} alt='Ilustração de perguntas e respostas' />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo real</p>
             </aside>
             <main>
-                <div>
+                <div className='main-content'>
                     <img src={logoImg} alt='Letmeask' />
                     <button>
                         <img src={googleIconImg} alt='Logo do Google' />
                         Crie sua sala com o Google
                     </button>
-                    <div>Ou entre em uma sala</div>
+                    <div
+                    className='separator'>Ou entre em uma sala</div>
                     <form>
                         <input
-                            type='submit'
+                            type='text'
                             placeholder='Digite o código da sala'
                         />
                         <button type='submit'>
